88b6c5b1dc9193916e6e75baac77ce3f67a066a1
CHAE <chae4582@gmail.com>
2023-09-27 19:09:00 +0900

    ㅎㅎ

--- moniet_web_back/src/main/java/kr/or/iei/MonietWebBackApplication.java
+++ moniet_web_back/src/main/java/kr/or/iei/MonietWebBackApplication.java
@@ -2,8 +2,11 @@ package kr.or.iei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
 public class MonietWebBackApplication {
 
 	public static void main(String[] args) {
