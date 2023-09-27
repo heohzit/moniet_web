b16ef5de9066ce26c04afcaf76bdabf4902b6334
heohzit <heohzit@gmail.com>
2023-09-27 15:14:03 +0900

    기본세팅

---
+++ moniet_web_back/src/main/java/kr/or/iei/MonietWebBackApplication.java
@@ -0,0 +1,13 @@
+package kr.or.iei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonietWebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonietWebBackApplication.class, args);
+	}
+
+}
