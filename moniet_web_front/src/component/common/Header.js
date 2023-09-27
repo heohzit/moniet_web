57a4722ffda8d58c36ecdc48adbac7463b776c11
heohzit <heohzit@gmail.com>
2023-09-27 17:46:55 +0900

    메뉴 링크

--- moniet_web_front/src/component/common/Header.js
+++ moniet_web_front/src/component/common/Header.js
@@ -1,8 +1,36 @@
+import { Link } from "react-router-dom";
+import "./default.css";
 const Header = () => {
   return (
     <header className="header-wrap">
-      <div>여기는 header</div>
+      <div className="header-title">
+        <Link to="/">머니어터</Link>
+      </div>
+      <NaviSide></NaviSide>
     </header>
+  );
+};
+const NaviSide = () => {
+  return (
+    <div className="nav-side">
+      <ul>
+        <li>
+          <Link to="#">대시보드</Link>
+        </li>
+        <li>
+          <Link to="#">내역</Link>
+        </li>
+        <li>
+          <Link to="#">달력</Link>
+        </li>
+        <li>
+          <Link to="#">머니챌린지</Link>
+        </li>
+        <li>
+          <Link to="#">커뮤니티</Link>
+        </li>
+      </ul>
+    </div>
   );
 };
 export default Header;
