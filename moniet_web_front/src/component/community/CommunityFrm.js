20c56088a95016c1cd3a510331723c752b9a8a35
naiana51 <92916376+naiana51@users.noreply.github.com>
2023-10-04 16:56:23 +0900

    react-quill install

--- moniet_web_front/src/component/community/CommunityFrm.js
+++ moniet_web_front/src/component/community/CommunityFrm.js
@@ -1,9 +1,81 @@
+import Input from "../util/InputFrm";
 import "./community.css";
 
-const CommunityFrm = () => {
+const CommunityFrm = (props) => {
+  const communityTitle = props.communityTitle;
+  const setCommunityTitle = props.setCommunityTitle;
+  const communitySubTitle = props.communitySubTitle;
+  const setCommunitySubTitle = props.setCommunitySubTitle;
+  const thumbnail = props.thumbnail;
+  const setThumbnail = props.setThumbnail;
+  const communityContent = props.communityContent;
+  const setCommunityContent = props.setCommunityContent;
+  const communityImg = props.communityImg;
+  const setCommunityImg = props.setCommunityImg;
+
+  const thumbnailChange = (e) => {};
+
   return (
-    <div>
-      <div>커뮤니티 폼</div>
+    <div className="community-frm-wrap">
+      <div className="community-frm-top">
+        <div className="community-thumbnail">
+          {communityImg === null ? (
+            <img src="/image/default.png" />
+          ) : (
+            <img src={communityImg} />
+          )}
+        </div>
+
+        <div className="community-info">
+          <table className="community-info-tbl">
+            <tbody>
+              <tr>
+                <td>
+                  <label htmlFor="communityTitle">제목</label>
+                </td>
+                <td>
+                  <Input
+                    type="text"
+                    data={communityTitle}
+                    setData={setCommunityTitle}
+                    content="communityTitle"
+                  />
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <label htmlFor="communitySubTitle">소제목</label>
+                </td>
+                <td>
+                  <Input
+                    type="text"
+                    data={communitySubTitle}
+                    setData={setCommunitySubTitle}
+                    content="communitySubTitle"
+                  />
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <label htmlFor="thumbnail">대표이미지</label>
+                </td>
+                <td>
+                  <input
+                    type="file"
+                    id="thumbnail"
+                    accept="image/*"
+                    onChange={thumbnailChange}
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div className="community-content-box"></div>
     </div>
   );
 };
