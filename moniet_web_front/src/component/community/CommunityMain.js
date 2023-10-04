0f8d8bd36aca72b04f907b104ec2cbb5ae26abd4
naiana51 <92916376+naiana51@users.noreply.github.com>
2023-10-04 11:14:03 +0900

    community react 수정

--- moniet_web_front/src/component/community/CommunityMain.js
+++ moniet_web_front/src/component/community/CommunityMain.js
@@ -1,11 +1,16 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./community.css";
+import CommunityView from "./CommunityView";
+import CommunityWrite from "./CommunityWrite";
 
 const CommunityMain = () => {
   return (
     <div className="community-all-wrap">
       <div className="community-title">커뮤니티</div>
-      <Routes></Routes>
+      <Routes>
+        <Route path="write" element={<CommunityWrite />} />
+        <Route path="view" element={<CommunityView />} />
+      </Routes>
     </div>
   );
 };
