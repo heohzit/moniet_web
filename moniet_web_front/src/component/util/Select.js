c75a0caa67b6c242ed088fd912cde0ceb28d461d
naiana51 <92916376+naiana51@users.noreply.github.com>
2023-10-04 16:31:12 +0900

    community list css 수정

---
+++ moniet_web_front/src/component/util/Select.js
@@ -0,0 +1,14 @@
+import "./select.css";
+
+const Select = (props) => {
+  return (
+    <>
+      <select>
+        <option value={1}>제목</option>
+        <option value={2}>작성자</option>
+      </select>
+    </>
+  );
+};
+
+export default Select;
