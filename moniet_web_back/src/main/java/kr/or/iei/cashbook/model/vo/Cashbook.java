a0979186d8bfbd20d7a13ac505b1cc17de75404c
CHAE <chae4582@gmail.com>
2023-10-13 19:07:48 +0900

    cashbook_tbl & 저축챌린지 카테고리

--- moniet_web_back/src/main/java/kr/or/iei/cashbook/model/vo/Cashbook.java
+++ moniet_web_back/src/main/java/kr/or/iei/cashbook/model/vo/Cashbook.java
@@ -23,12 +23,16 @@ public class Cashbook {
 	private int cashbookMoney;
 	private String cashbookContent;
 	private String cashbookMemo;
+	private int challengeNo;
 	
 	//화면 노출용
 	private String memberId;
 	private String categoryTitle;
 	
+	
 	//조회용
 	private String startDate;
 	private String endDate;
+	
+	
 }
